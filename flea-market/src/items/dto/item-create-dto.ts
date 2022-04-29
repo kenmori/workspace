c2356372723e851cd127f30525236b0cbcf95885
森田賢二 <kenjimorita@moritakenjinoMacBook-Pro.local>
2022-04-30 00:13:37 +0900

    fix: global validator

--- flea-market/src/items/dto/item-create-dto.ts
+++ flea-market/src/items/dto/item-create-dto.ts
@@ -1,8 +1,21 @@
+import { IsString, MaxLength, IsNotEmpty, IsInt, Min } from "class-validator"
+import { Type } from "class-transformer"
+import { isInt16Array } from "util/types"
 import { ItemStatus } from "../item-status.enum"
 
 export class CreateItemDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(40)
   name: string
+
+  @IsInt()
+  @Min(1)
+  @Type(() => Number) //リクエストが文字列で渡ってくることがあるのでプロパティをNumber型として認識する様にする
   price: number
+
+  @IsString()
+  @IsNotEmpty()
   description: string
   status: ItemStatus
 }
