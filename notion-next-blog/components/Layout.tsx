3fd22616d6e2c7737cc964d8988e40e554ff463b
森田賢二 <kenjimorita@moritakenjinoMacBook-Pro.local>
2022-07-30 22:32:58 +0900

    fix

--- notion-next-blog/components/Layout.tsx
+++ notion-next-blog/components/Layout.tsx
@@ -1,16 +1,20 @@
 import Link from 'next/link'
-import React from 'react'
+import React, {FC} from 'react'
+import { LayoutProps } from '../types/types'
 import Footer from './Footer'
 import Navibar from './Navibar'
 
-export default function Layout() {
+
+const Layout:FC<LayoutProps> = ({children}) => {
   return (
     <div>
   {/** */}
   <Navibar />
-  <main></main>
+  <main className="w-full pb-12 px-4">{children}</main>
   {/** */}
   <Footer/>
     </div>
   )
 }
+
+export default Layout
