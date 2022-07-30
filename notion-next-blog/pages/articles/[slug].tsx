38430ad04d2cdb136213ee925c8bd71551e33881
森田賢二 <kenjimorita@moritakenjinoMacBook-Pro.local>
2022-07-30 23:12:24 +0900

    fix

--- notion-next-blog/pages/articles/[slug].tsx
+++ notion-next-blog/pages/articles/[slug].tsx
@@ -1,30 +1,35 @@
-import { GetServerSideProps } from "next";
+import { GetServerSideProps, NextPage } from "next";
 import Image from "next/image";
 import React from "react";
 import ArticleMeta from "../../components/ArticleMeta";
 import Layout from "../../components/Layout";
-import { Params } from "../../types/types";
+import { AirticleProps, Params } from "../../types/types";
+import { sampleCards } from "../../utils/sample";
 
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const {slug} = ctx.params as Params // as [slug].tsx
+  const page = sampleCards.find((card) => card.slug === slug)
+
   return {
     props: {
-      slug
+      page
     }
   }
 }
-const Article = ({slug}) => {
+
+// NextPage Page型
+const Article: NextPage<AirticleProps> = ({page}) => {
   return (
     <Layout>
       <article className="w-full">
         {/* meta section */}
         <div className="my-12">
-          <ArticleMeta />
+          <ArticleMeta page={page} />
         </div>
 
         {/* article */}
-        <div className="my-12">article {slug}</div>
+        <div className="my-12">article {page.author}</div>
       </article>
     </Layout>
   );
